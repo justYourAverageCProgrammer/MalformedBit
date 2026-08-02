cb65696afbf254cfce3c81f4e2f7388c33268e9c
justYourAverageCProgrammer <orh2020@outlook.com>
2026-08-02 16:17:29 -0400

    self-printing quine

--- Main.c
+++ Main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-  printf("hello world\n");
+  const char* code = "#include <stdio.h>%c%cint main(void){%c const char* code = %c%s%c;%c  printf(code, 10, 10, 10, 34, code, 34, 10, 10, 10);%c  return 0;%c}";
+  printf(code, 10, 10, 10, 34, code, 34, 10, 10, 10);
   return 0;
 }
